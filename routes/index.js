cc20f02073e939c3ff561d2d30e0711deed98de4
yifu <yifuxia@usc.edu>
2017-03-09 21:00:06 -0800

    Youtube project added

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'A5-Yifu_Xia' });
+  res.render('index', { title: 'Yifu_Xia' });
 });
 
 module.exports = router;
